302fcbd8585b26934ce4e2e945e213595af06c14
AYSMN <ayushmanmohanty2007@gmail.com>
2025-05-29 03:24:24 +0530

    added basic functionality

--- helloworld.py
+++ helloworld.py
@@ -3,4 +3,15 @@ print("hello world")
 def greet(name):
     name = input("hi, what's your name : ")
     print("hello, "+ name)
-    
+
+
+
+for i in range(0, 21, 2):
+        print(i)
+
+count = 0
+while count < 5:
+    print("Count is:", count)
+    count += 1
+
+print("This is a simple Python script to demonstrate basic functionality.")
